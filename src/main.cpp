350793e50b11897489e23053861ac8b0fe3bb9f9
weixinkai <w2724601283@163.com>
2025-09-02 12:18:24 +0800

    feat:implement logic thread and render thread

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,33 @@ struct GameState {
 };
 
 int main() {
+    AsyncLogger logger;
+    DoubleBuffer<GameState> gameBuffer;
+
     std::atomic<bool> running(true);
 
     // 游戏逻辑线程
     std::thread logicThread([&]() {
         int frame = 0;
         while (running) {
+            auto& writeBuffer = gameBuffer.getWriteBuffer();
+            writeBuffer.frameNumber = frame++;
+            writeBuffer.entityPositions = { frame, frame + 1, frame + 2 };
+
+            logger.log("Logic updated frame " + std::to_string(writeBuffer.frameNumber));
+
+            gameBuffer.swapBuffers();
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
     });
 
     // 渲染线程
     std::thread renderThread([&]() {
         while (running) {
+            const auto& readBuffer = gameBuffer.getReadBuffer();
+            logger.log("Render frame " + std::to_string(readBuffer.frameNumber) +
+                       " Entities: " + std::to_string(readBuffer.entityPositions.size()));
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
     });
 
